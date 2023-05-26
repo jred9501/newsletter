c4c94bd9ec57fc96aff170ef2b83c2d0a71468aa
jred9501 <jredondo9501@gmail.com>
2023-05-26 13:07:41 -0700

    navbar completion

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,64 @@
-import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "./Logo";
+import NavItem from "./NavItem";
 
-function Navbar() {
+const MENU_LIST = [
+  { text: "Home", href: "/home" },
+  { text: "About Us", href: "/about" },
+  { text: "Index", href: "/" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
   return (
-    <ul>
-      <li>
-        <Link href="/home">Home</Link>
-      </li>
-      <li>
-        <Link href="/about">About</Link>
-        </li>
-        <li>
-       
-          <Link href="/">Index</Link>
-       
-      </li>
-    </ul>
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <h1 className="logo">News Feed</h1>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
   );
-}
+};
 
 export default Navbar;
+
+{
+  /* <ul>
+<li>
+  <Link href="/home">Home</Link>
+</li>
+<li>
+  <Link href="/about">About</Link>
+  </li>
+  <li>
+ 
+    <Link href="/">Index</Link>
+ 
+</li>
+</ul> */
+}
