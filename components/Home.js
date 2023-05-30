668a287a8a657b9542de42ebd2fc5e0c78e49a07
jred9501 <jredondo9501@gmail.com>
2023-05-30 12:15:39 -0700

    added tailwind and typescript

--- components/Home.js
+++ components/Home.js
@@ -1,27 +1,28 @@
+import Head from "next/head";
+
 import Image from "next/image";
 import image from "../images/pic.jpg";
 
 function Home() {
   return (
     <div className="jumbotron">
-      <div
-        id="carouselExampleSlidesOnly"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
+      <Head>
+        <title>News Feed</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        ></link>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+      </Head>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <Image src={image} className="d-block w-50" alt="..." />
+          <button type='button' className="btn btn-success">Success</button>
           </div>
-          <div className="carousel-item active">
-            <Image src={image} className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item active">
-            <Image src={image} className="d-block w-100" alt="..." />
-          </div>
+      
         </div>
       </div>
-    </div>
+ 
   );
 }
 
