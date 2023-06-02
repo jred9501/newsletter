de85fc34469a301ae89128170ed12a1c6a6d5e25
jred9501 <jredondo9501@gmail.com>
2023-06-02 09:26:59 -0700

    newest layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import "../styles/globals.css"; // this is the global css file
+import Head from "next/head";
 
 import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>News Feed</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        ></link>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> */}
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
--- pages/home.js
+++ pages/home.js
@@ -4,23 +4,32 @@ import image from "../public/pic.jpg";
 
 function home() {
   return (
-   
-    <div className="container">
-    <div className="name-box">
-      <Image src={image} className="header-img img img-responsive" alt="..."  />
-    </div>
-    <div className="main">
-      this is the main content
-    </div>
-    <div id="sidebar">
-
-     
-    </div>
- 
+    <div class="container">
+    <nav>Navbar</nav>
+    <main>Main</main>
+    <div id="sidebar">Sidebar</div>
+    <div id="content1">Content1</div>
+    <div id="content2">Content2</div>
+    <div id="content3">Content3</div>
     <footer>Footer</footer>
   </div>
+  
    
   );
 }
 
 export default home;
+//   <div className="container">
+//     <div className="name-box">
+//       <Image src={image} className="header-img img img-responsive" alt="..."  />
+//     </div>
+//     <div className="main">
+//       this is the main content
+//     </div>
+//     <div id="sidebar">
+
+     
+//     </div>
+ 
+//     <footer>Footer</footer>
+//   </div>
