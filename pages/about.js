fc1b46d24b6af58933c177649954a5308bd1830d
jred9501 <jredondo9501@gmail.com>
2023-06-06 09:31:30 -0700

    added api

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,51 @@
 import React from "react";
 
-
-function About(){
-    return(
-        <div>
-            <h1>About</h1>
-        </div>
-    )
+function About() {
+  return (
+    <div className="container2">
+      <div className="about-us-wrapper">
+        <h1>About Us</h1>
+      </div>
+      <div className="about-main">
+        <p>
+        Welcome to the heart-throbbing world of Breaking Pulse, where news beats
+        with an unrelenting fierceness. We are the rebels of information, the
+        pioneers of truth, and the champions of the untold stories that deserve
+        to be heard. In a world drowning in mediocrity, we rise above the
+        mundane to bring you the most electrifying and badass news content. Our
+        team of intrepid journalists fearlessly ventures into the darkest
+        corners of society, unearthing the hidden gems that ignite passion and
+        challenge the status quo. We believe in the power of storytelling—the
+        kind that rouses your spirit, quickens your pulse, and sets your heart
+        ablaze. Our words are crafted with precision, capturing the raw essence
+        of the human experience and leaving an indelible mark on your soul. From
+        the neon-lit streets where revolution simmers to the hallowed halls of
+        power where secrets lurk, Breaking Pulse is your gateway to the untamed,
+        the audacious, and the extraordinary. We are unafraid to delve into the
+        forbidden, shedding light on the untold stories that demand justice and
+        change. Our news feed is a pulsating tapestry of captivating narratives,
+        fearless investigations, and heart-wrenching tales that will make you
+        question, inspire you to act, and ignite the fire within you. We bring
+        you the triumphs of everyday heroes, the battles against injustice, and
+        the unyielding resilience of the human spirit. At Breaking Pulse, we
+        don't just report the news; we ignite a revolution. Our platform is a
+        battleground, where truth battles falsehood, and where information is a
+        weapon in the hands of those who seek to make a difference. We empower
+        you, our readers, to challenge the status quo, to question the
+        narrative, and to join the ranks of those who dare to make a stand. So,
+        strap yourself in and brace for impact. Join us on this heart-throbbing
+        journey where the news is unfiltered, the stories are raw, and the truth
+        will leave you breathless. Welcome to Breaking Pulse—where bad-ass
+        journalism meets the beating pulse of a world hungry for change.
+        </p>
+      </div>
+      <div id="sidebar">Sidebar</div>
+      <div id="content1">Content1</div>
+      <div id="content2">Content2</div>
+      <div id="content3">Content3</div>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 export default About;
--- pages/home.js
+++ pages/home.js
@@ -1,24 +1,49 @@
-import React from "react";
-import Image from "next/image";
-import image from "../public/pic.jpg";
+import { useEffect, useState } from "react";
 
-function home() {
+export default function Home() {
+  const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  const fetchNews = async () => {
+    try {
+      const response = await fetch(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=2d3eb0d29c8d4341a0e551011ea58999"
+      );
+      const data = await response.json();
+      setNewsData(data.articles);
+    } catch (error) {
+      console.log("Error fetching news:", error);
+    }
+  };
+
   return (
     <div className="container">
-    <div className="home-page-header">divbar</div>
-    <main>Main</main>
-    <div id="sidebar">Sidebar</div>
-    <div id="content1">Content1</div>
-    <div id="content2">Content2</div>
-    <div id="content3">Content3</div>
-    <footer>Footer</footer>
-  </div>
-  
-   
+      <div className="home-page-header"></div>
+      <main>
+        {/* Your page content */}
+        {newsData.map((newsItem, index) => (
+          <div key={index}>
+            <h3>{newsItem.title}</h3>
+            <p>{newsItem.description}</p>
+            <img src={newsItem.image} alt={newsItem.title} />
+            {/* Render other news data fields */}
+          </div>
+        ))}
+      </main>
+
+      <div id="sidebar">Sidebar</div>
+      <div id="content1">Content1</div>
+      <div id="content2">Content2</div>
+      <div id="content3">Content3</div>
+      <footer>Footer</footer>
+    </div>
   );
 }
 
-export default home;
+// export default home;
 //   <div className="container">
 //     <div className="name-box">
 //       <Image src={image} className="header-img img img-responsive" alt="..."  />
@@ -28,8 +53,22 @@ export default home;
 //     </div>
 //     <div id="sidebar">
 
-     
 //     </div>
- 
+
+//     <footer>Footer</footer>
+//   </div>function home() {
+
+//   return (
+//     <div className="container">
+//     <div className="home-page-header">
+//     </div>
+//     <main>Main</main>
+//     <div id="sidebar">Sidebar</div>
+//     <div id="content1">Content1</div>
+//     <div id="content2">Content2</div>
+//     <div id="content3">Content3</div>
 //     <footer>Footer</footer>
 //   </div>
+
+//   );
+// }
